c1dbdc5e8d2fcdfc92cfed08bd2d4e82bd9dc8fe
Satoko <satoko@suzuki.email.ne.jp>
2023-12-29 18:45:53 +0900

    印刷ボタンに説明追加

--- components/common/PrintOutBtn.vue
+++ components/common/PrintOutBtn.vue
@@ -1,6 +1,12 @@
 <template>
     <div :class="$style.container">
-        <v-btn @click="printOut">印刷</v-btn>
+        <client-only>
+            <v-tooltip activator="" :text="explain">
+                <template v-slot:activator="{ props }">
+                    <v-btn @click="printOut" v-bind="props">印刷</v-btn>
+                </template>
+            </v-tooltip>
+        </client-only>
     </div>
 </template>
 
@@ -10,12 +16,22 @@ import { defineComponent } from 'vue';
 export default defineComponent({
     name: 'PrintOutBtn',
     setup() {
+        const route = useRoute();
+
         const printOut = () => {
             window.print();
         }
 
+        const explain = computed(() => {
+            const message = '印刷プレビュー画面を表示します。'
+            if (route.path === '/monthly') {
+                return message + '「背景のグラフィックス」を印刷するように指定してください。'
+            }
+            return message;
+        })
         return {
             printOut,
+            explain,
         }
     }
 });
