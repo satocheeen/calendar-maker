8fbdb80914dd8a0ef0b12c2e62abbcb728cc5684
Satoko <satoko@suzuki.email.ne.jp>
2023-12-29 17:11:21 +0900

    GoogleFontを読み込めるように対応

--- components/common/CalendarViewLayout.vue
+++ components/common/CalendarViewLayout.vue
@@ -11,9 +11,24 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+// @ts-ignore
+import webFontLoader from 'webfontloader';
+import { WebFontNameList } from '~/store/font_define';
 
 export default defineComponent({
-    name: 'CalendarViewLayout'
+    name: 'CalendarViewLayout',
+    setup() {
+        onMounted(() => {
+          if (process.client) {
+            webFontLoader.load({
+              google: {
+                families: WebFontNameList.map(font => (font.replaceAll(' ', '+') + ':400,700'))
+              }
+            })
+          }
+        })
+
+    }
 });
 </script>
 
