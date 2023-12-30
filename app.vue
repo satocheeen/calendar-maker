c9c6d4ff1ea1b56aed6990584862167ba3393e57
Satoko <satoko@suzuki.email.ne.jp>
2023-12-30 11:53:52 +0900

    Webフォントロードができていない問題の対処

--- app.vue
+++ app.vue
@@ -1,31 +1,43 @@
 <template>
-  <v-app>
-      <v-main :class="$style.container">
-          <menu-bar :class="$style.menu" />
-          <div :class="$style.main">
-            <NuxtPage />
-          </div>
-      </v-main>
-      <WebFontLoader />
-  </v-app>
+    <v-app>
+        <v-main :class="$style.container">
+            <menu-bar :class="$style.menu" />
+            <div :class="$style.main">
+                <NuxtPage />
+            </div>
+        </v-main>
+    </v-app>
 </template>
 
 <script lang="ts">
 import MenuBar from './components/MenuBar.vue';
 import useStyle, { StyleStoreKey } from './store/useStyle';
 import useOperation, { OperationStoreKey } from './store/useOperation';
+import { WebFontNameList } from './store/font_define';
 
 export default defineComponent({
+    components: {
+        MenuBar,
+    },
     setup() {
+      onMounted(async() => {
+          if (process.client) {
+            // webfontloaderはクライアントサイドでなければimport失敗する
+            // @ts-ignore
+            const webFontLoader = await import('webfontloader') as any;
+            webFontLoader.load({
+              google: {
+                families: WebFontNameList.map(font => (font.replaceAll(' ', '+') + ':400,700'))
+              }
+            })
+          }
+        })
+
         const operationStore = useOperation();
         provide(OperationStoreKey, operationStore);
         provide(StyleStoreKey, useStyle( { operation: operationStore }));
         return {};
     },
-    components: {
-        MenuBar,
-        WebFontLoader: () => process.client && import('@/components/common/WebFontLoader.vue'),
-    }
 })
 </script>
 
@@ -52,21 +64,21 @@ html::-webkit-scrollbar {
 
 <style lang="scss" module>
 .container {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
 }
 .main {
-  flex: 1;
-  height: calc(100vh - 50px);
+    flex: 1;
+    height: calc(100vh - 50px);
 }
 .menu {
-  height: 50px;
+    height: 50px;
 }
 @media print {
-  .menu {
-    display: none;
-  }
+    .menu {
+      display: none;
+    }
 }
 
 </style>
